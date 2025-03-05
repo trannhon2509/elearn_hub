e362837a08aec6320b89f2545ead7aa85ae92f76
trannhon2509 <nhonvn2509@gmail.com>
2025-03-05 07:58:00 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import DashboardLayout from './layouts/DashboardLayout'
+import Dashboard from './pages/Dashboard'
+
+function App() {
+
+  return (
+    <>
+      <DashboardLayout>
+        <Dashboard />
+      </DashboardLayout>
+    </>
+  )
+}
+
+export default App
